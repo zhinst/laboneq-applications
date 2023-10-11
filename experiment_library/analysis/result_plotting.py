d6124af8cb3fb4b6ed3536779087bcab7797a518
Messner Andreas <andreas.messner@zhinst.com>
2023-10-11 15:13:44 +0200

    Improving result_plotting

--- experiment_library/analysis/result_plotting.py
+++ experiment_library/analysis/result_plotting.py
@@ -3,14 +3,14 @@ import numpy as np
 
 
 def plot_resonator_spectroscopy_full_range(results):
-    results_x = results.acquired_results.to_xarray()
+    results_x = results.acquired_results["resonator_spectroscopy"].to_xarray()
 
     plt.figure()
     for i in range(results_x.sizes["sweep_0"]):
         plt.plot(
-            results_x.data_vars["resonator_spectroscopy"][i]["sweep_0"]
-            + results_x.data_vars["resonator_spectroscopy"][i]["sweep_1"],
-            20 * np.log10(np.abs(results_x.data_vars["resonator_spectroscopy"][i])),
+            results_x[i]["sweep_0"]
+            + results_x[i]["sweep_1"],
+            20 * np.log10(np.abs(results_x[i])),
         )
     plt.grid()
     plt.xlabel("Frequency [Hz]")
