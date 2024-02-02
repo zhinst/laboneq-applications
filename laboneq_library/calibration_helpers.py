4a588d9709517bd56243fdcb408bdf5760e53d6f
Markus Heikkinen <markus.heikkinen@zhinst.com>
2024-02-02 10:56:46 +0100

    Remove legacy code

--- laboneq_library/calibration_helpers.py
+++ laboneq_library/calibration_helpers.py
@@ -1,117 +1,14 @@
-import time
 import json
 import os
-from pathlib import Path
-
+import logging
 from laboneq.simple import *  # noqa: F403
-from ruamel.yaml import YAML
-
 from laboneq_library.qpu_types.tunable_transmon import (
     TunableTransmonQubit,
     TunableTransmonQubitParameters,
 )
 
-ryaml = YAML()
-
-import logging
 
 log = logging.getLogger(__name__)
-
-
-def update_qubit_parameters_and_calibration(
-    qubit_parameters,
-    device_setup,
-    calibration_file="./qubit_parameters.yaml",
-    history_path="calib_history",
-    set_local_oscillators=True,
-):
-    with open(calibration_file, "w") as file:
-        ryaml.dump(qubit_parameters, file)
-
-    Path(history_path).mkdir(parents=True, exist_ok=True)
-
-    timestamp = str(time.strftime("%Y-%m-%d_%H%M%S"))
-    history_filename = history_path + "/" + timestamp + "calib.yaml"
-    with open(history_filename, "w") as file:
-        ryaml.dump(qubit_parameters, file)
-
-    transmon_list = []
-    for qubit in qubit_parameters["qubits"]:
-        transmon = create_transmon(qubit, qubit_parameters, device_setup)
-        device_setup.set_calibration(
-            transmon.calibration(set_local_oscillators=set_local_oscillators)
-        )
-        transmon_list.append(transmon)
-    return transmon_list
-
-
-# create a transmon qubit object from entries in a parameter dictionary
-def create_transmon(qubit: str, base_parameters, device_setup):
-    q_name = qubit
-    transmon = TunableTransmonQubit.from_logical_signal_group(
-        q_name,
-        lsg=device_setup.logical_signal_groups[q_name],
-        parameters=TunableTransmonQubitParameters(
-            resonance_frequency_ge=base_parameters["qubits"][qubit][
-                "resonance_frequency_ge"
-            ]["value"],
-            resonance_frequency_ef=base_parameters["qubits"][qubit][
-                "resonance_frequency_ef"
-            ]["value"],
-            drive_lo_frequency=base_parameters["qubits"][qubit]["drive_lo_frequency"][
-                "value"
-            ],
-            readout_resonator_frequency=base_parameters["qubits"][qubit][
-                "readout_resonator_frequency"
-            ]["value"],
-            readout_lo_frequency=base_parameters["qubits"][qubit][
-                "readout_lo_frequency"
-            ]["value"],
-            readout_integration_delay=base_parameters["qubits"][qubit][
-                "readout_integration_delay"
-            ]["value"],
-            drive_range=base_parameters["qubits"][qubit]["drive_range_ge"]["value"],
-            readout_range_out=base_parameters["qubits"][qubit]["readout_range_out"][
-                "value"
-            ],
-            readout_range_in=base_parameters["qubits"][qubit]["readout_range_in"][
-                "value"
-            ],
-            flux_offset_voltage=base_parameters["qubits"][qubit]["dc_source"]["value"],
-            user_defined={
-                "amplitude_pi": base_parameters["qubits"][qubit]["amplitude_pi"][
-                    "value"
-                ],
-                "amplitude_pi2": base_parameters["qubits"][qubit]["amplitude_pi2"][
-                    "value"
-                ],
-                "amplitude_pi_ef": base_parameters["qubits"][qubit]["amplitude_pi_ef"][
-                    "value"
-                ],
-                "amplitude_pi2_ef": base_parameters["qubits"][qubit][
-                    "amplitude_pi2_ef"
-                ]["value"],
-                "drive_range_ef": base_parameters["qubits"][qubit]["drive_range_ef"][
-                    "value"
-                ],
-                "pulse_length": base_parameters["qubits"][qubit]["pulse_length"][
-                    "value"
-                ],
-                "readout_length": base_parameters["qubits"][qubit]["readout_length"][
-                    "value"
-                ],
-                "readout_amplitude": base_parameters["qubits"][qubit][
-                    "readout_amplitude"
-                ]["value"],
-                "reset_delay_length": base_parameters["qubits"][qubit][
-                    "reset_delay_length"
-                ]["value"],
-                "dc_slot": base_parameters["qubits"][qubit]["dc_source"]["slot"],
-                "cr_frequency": base_parameters["qubits"][qubit]["cr_freq"]["value"],
-            },
-        ),
-    )
-    return transmon
 
 
 def update_measurement_setup_from_qubits(qubits, measurement_setup):
@@ -133,65 +30,6 @@ def update_measurement_setup_from_qubits(qubits, measurement_setup):
             qb_idx = measurement_setup.qubits.index(qubit)
             measurement_setup.qubits[qb_idx] = qubit
     measurement_setup.set_calibration(msmt_setup_cal)
-
-
-def create_qubits_from_measurement_setup(measurement_setup):
-    """
-    Instantiates TunableTransmonQubits from the logical signals in measurement_setup and
-    dome default values for the parameters.
-
-    Args:
-        measurement_setup: instance of DeviceSetup
-
-    Returns:
-        list of TunableTransmonQubit instances
-    """
-
-    qubits = []
-    for qb_name in measurement_setup.logical_signal_groups:
-        qubits += [
-            TunableTransmonQubit.from_logical_signal_group(
-                uid=qb_name,
-                lsg=measurement_setup.logical_signal_groups[qb_name],
-                parameters=TunableTransmonQubitParameters(
-                    resonance_frequency_ge=6.00e9,
-                    resonance_frequency_ef=5.83e9,
-                    drive_lo_frequency=5.90e9,
-                    readout_resonator_frequency=7.00e9,
-                    readout_lo_frequency=7.30e9,
-                    readout_integration_length=2e-06,
-                    readout_integration_delay=2.4e-07,
-                    drive_range=10,
-                    readout_range_out=-25,
-                    readout_range_in=-5,
-                    flux_offset_voltage=0,
-                    drive_parameters_ge={
-                        "amplitude_pi": 0.30,
-                        "amplitude_pi2": 0.15,
-                        "beta": 0,
-                        "length": 5e-08,
-                        "sigma": 0.2,
-                    },
-                    drive_parameters_ef={
-                        "amplitude_pi": 0.30,
-                        "amplitude_pi2": 0.15,
-                        "beta": 0,
-                        "length": 5e-08,
-                        "sigma": 0.2,
-                    },
-                    user_defined={
-                        "dc_slot": 0,
-                        "dc_voltage_parking": 0,
-                        "readout_amplitude": 0.5,
-                        "readout_length": 2e-06,
-                        "reset_delay_length": 1e-06,
-                        "spec_amplitude": 1,
-                        "spec_length": 5e-06,
-                    },
-                ),
-            )
-        ]
-    return qubits
 
 
 def create_qubits_from_parameters(qubit_parameters, measurement_setup):
