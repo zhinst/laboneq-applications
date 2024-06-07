5b14c8f2209e15e0c15801508b935c738f015661
Markus Heikkinen <markus.heikkinen@zhinst.com>
2024-06-07 09:57:23 +0200

    Add TaskBook to workflow top level

--- src/laboneq_applications/workflow/__init__.py
+++ src/laboneq_applications/workflow/__init__.py
@@ -4,6 +4,6 @@ This package provides tools and building blocks to define workflows.
 """
 
 from laboneq_applications.workflow.task import task
-from laboneq_applications.workflow.taskbook import taskbook
+from laboneq_applications.workflow.taskbook import TaskBook, taskbook
 
-__all__ = ["task", "taskbook"]
+__all__ = ["task", "taskbook", "TaskBook"]
