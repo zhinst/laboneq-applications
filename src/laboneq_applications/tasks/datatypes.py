c96722bb848ade53b2c2db4fe21debe09481aaef
Jan Lienemann <jan.lienemann@zhinst.com>
2024-07-12 10:12:09 +0000

    Pretty print RunExperimentResults and AttributeWrapper

--- src/laboneq_applications/tasks/datatypes.py
+++ src/laboneq_applications/tasks/datatypes.py
@@ -2,13 +2,133 @@
 
 from __future__ import annotations
 
-from collections.abc import Iterator, Mapping
+from collections.abc import Collection, ItemsView, Iterable, Iterator, ValuesView
 from collections.abc import KeysView as BaseKeysView
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, Any
+from io import StringIO
+from typing import TYPE_CHECKING, Any, Protocol, TypeVar
+
+from typing_extensions import ParamSpec
 
 if TYPE_CHECKING:
     from numpy.typing import ArrayLike
+
+use_rich_pprint: bool = True
+try:
+    from rich.console import Console
+    from rich.pretty import pprint as rich_pprint
+except ImportError:
+    from pprint import pprint as pprint_pprint
+
+    use_rich_pprint = False
+
+
+class HasAsStrDict(Protocol):
+    """A protocol for classes that have a method to return a dictionary representation.
+
+    The dictionary representation is used to pretty-print the object using the pprint
+    function. The dictionary should contain all attributes that should be printed.
+    Not to be confused with the __dict__ attribute, which contains the actual attributes
+    of the object, including methods and private attributes.
+    """
+
+    def _as_str_dict(self) -> dict[str, Any]: ...
+
+
+T = TypeVar("T", bound=HasAsStrDict)
+P = ParamSpec("P")
+
+
+def classformatter(cls: type[T]) -> type[T]:
+    """A decorator to customize the string representation of class instances.
+
+    This decorator overwrites the __str__ and __format__ methods of the decorated class.
+    The new __str__ method pretty-prints the instance using the pprint function,
+    ensuring a visually appealing output on compatible terminals. The __format__ method
+    is overridden to return the class's original __repr__ representation. Also,
+    the `_repr_pretty_` method is added to support pretty-printing in Jupyter notebooks.
+
+    In contrast to the similar decorator with the same name from laboneq, this creates
+    the string representation based on a dictionary provided by the class'
+    (   )`_as_str_dict()`.
+
+    If the global variable `use_rich_pprint` is `True`, the rich library will be used,
+    otherwise `pprint.pprint`.
+
+    Args:
+        cls (type): The class to be decorated.
+
+    Returns:
+        type: The decorated class with modified __str__ and __format__ methods.
+    """
+
+    def new_str(self: T) -> str:
+        as_dict = self._as_str_dict()
+        with StringIO() as buffer:
+            if use_rich_pprint:
+                console = Console(file=buffer)
+                rich_pprint(
+                    as_dict,
+                    console=console,
+                    expand_all=True,
+                    indent_guides=True,
+                )
+            else:
+                pprint_pprint(as_dict, stream=buffer)  # noqa: T203
+            return buffer.getvalue()
+
+    def new_format(self: T, _: object) -> str:
+        return format(self._as_str_dict())
+
+    def repr_pretty(self, p, _cycle):  # noqa: ANN001, ANN202
+        # For Notebooks
+        p.text(str(self))
+
+    cls.__str__ = new_str
+    cls.__format__ = new_format
+    cls._repr_pretty_ = repr_pretty
+
+    return cls
+
+
+def _check_prefix(keys: set[str], separator: str) -> None:
+    """Check if there is no key which is also a prefix of another.
+
+    Args:
+        keys: A set of keys.
+        separator: The symbol used to separate levels in the keys.
+
+    Raises:
+        ValueError: If there is a key which is also the prefix of another.
+    """
+    # Sort. Appending the separator helps to avoid cases if the key name
+    # contains a character which is sorts before the separator, like
+    # "a/a", "a/a/b" and "a/a.".
+    sorted_keys = sorted(k + separator for k in keys)
+    # Raise if a key is the prefix of the following key
+    for i in range(len(sorted_keys) - 1):
+        k1, k2 = sorted_keys[i], sorted_keys[i + 1]
+        if k2.startswith(k1):
+            raise ValueError(f"Key '{k1[:-1]}' is a prefix of '{k2[:-1]}'.")
+
+
+def _check_attrs(keys: set[str], attrs: list[str], separator: str) -> None:
+    """Check if no subkey matches an attribute of the class.
+
+    Args:
+        keys: A set of keys.
+        attrs: A list of attribute names.
+        separator: The symbol used to separate levels in the keys.
+
+    Raises:
+        ValueError: If a key is also an attribute of the class.
+    """
+    attrs_set = set(attrs)
+    subkeys = {subkey for key in keys for subkey in key.split(separator)}
+    if attrs_set & subkeys:
+        raise ValueError(
+            f"Handles {subkeys & attrs_set} aren't allowed names.",
+        )
 
 
 @dataclass
@@ -33,7 +153,8 @@ class AcquiredResult:
     axis: list[ArrayLike | list[ArrayLike]] = field(default_factory=list)
 
 
-class AttributeWrapper(Mapping[str, Any]):
+@classformatter
+class AttributeWrapper(Collection[str]):
     """A wrapper for accessing members of a dict in dot notation.
 
     Input data is a dict, where each key is a string, where levels are separated by
@@ -56,7 +177,7 @@ class AttributeWrapper(Mapping[str, Any]):
     ```
     """
 
-    class KeysView(BaseKeysView):
+    class KeysView(BaseKeysView[str]):
         """A view of the keys of an AttributeWrapper."""
 
         def __str__(self) -> str:
@@ -80,38 +201,65 @@ class AttributeWrapper(Mapping[str, Any]):
     def _add_path(self, key: str) -> str:
         return (self._path + self._separator + key) if self._path else key
 
-    def __init__(self, data: dict[str, Any], path: str | None = None) -> None:
+    def __init__(
+        self,
+        data: dict[str, Any] | None,
+        path: str | None = None,
+        separator: str | None = None,
+    ) -> None:
         super().__init__()
-        self._data = data
+        self._data = data or {}
         self._path = path or ""
-        self._separator = "/"
+        self._separator = separator if separator is not None else "/"
+        self._key_cache: set[str] = set()
+        keys_set = set(self._data.keys())
+        _check_attrs(keys_set, dir(self), self._separator)
+        _check_prefix(keys_set, self._separator)
         self._key_cache = {
             self._get_subkey(k) for k in self._data if k.startswith(self._path)
         }
-        if not self._key_cache:
-            raise KeyError(f"Key '{self._path}' not found in the data.")
 
-    # Mapping interface
+    # Partial Mapping interface
     def __len__(self) -> int:
         return len(self._key_cache)
 
     def __iter__(self) -> Iterator[str]:
         return iter(self._key_cache)
 
-    def __getitem__(self, key: str) -> AttributeWrapper:
+    def __getitem__(self, key: object) -> object:
+        if not isinstance(key, str):
+            raise TypeError(f"Key {key} has to be of type str.")
         path = self._add_path(key)
         try:
             return self._data[path]
-        except KeyError:
+        except KeyError as e:
+            path_sep = path + self._separator
+            if not any(k.startswith(path_sep) for k in self._data):
+                raise KeyError(f"Key '{self._path}' not found in the data.") from e
             return AttributeWrapper(self._data, path)
 
-    def keys(self) -> AttributeWrapper.KeysView[str]:
+    def __contains__(self, key: object) -> bool:
+        try:
+            self[key]
+        except KeyError:
+            return False
+        return True
+
+    def _keys(self) -> AttributeWrapper.KeysView:
         """A set-like object providing a view on the available attributes."""
         return AttributeWrapper.KeysView(self)
 
+    def _items(self) -> ItemsView[str, Any]:
+        """A set-like object providing a view on wrapper's items."""
+        return ItemsView(self)
+
+    def _values(self) -> ValuesView[Any]:
+        """An object providing a view on the wrapper's values."""
+        return ValuesView(self)
+
     # End of Mapping interface
 
-    def __getattr__(self, key: str) -> AttributeWrapper:
+    def __getattr__(self, key: str) -> object:
         try:
             return self.__getitem__(key)
         except KeyError as e:
@@ -128,22 +276,26 @@ class AttributeWrapper(Mapping[str, Any]):
             and self._separator == value._separator
         )
 
-    def __repr__(self) -> str:
-        return str(self)
+    def _as_str_dict(self) -> dict[str, Any]:
+        return {
+            key: (attr._as_str_dict() if isinstance(attr, AttributeWrapper) else attr)
+            for key, attr in ((key, getattr(self, key)) for key in self._key_cache)
+        }
 
-    def __str__(self) -> str:
-        return str(
-            {
-                self._get_subkeys(k): v
-                for k, v in self._data.items()
-                if k.startswith(self._path)
-            },
+    def __dir__(self) -> Iterable[str]:
+        return list(super().__dir__()) + list(self._key_cache)
+
+    def __repr__(self) -> str:
+        return (
+            f"AttributeWrapper(data={self._data!r}, path={self._path!r}, "
+            f"separator={self._separator!r})"
         )
 
-    def __dir__(self):
-        return super().__dir__() + list(self._key_cache)
+
+ErrorList = list[tuple[list[int], str, str]]
 
 
+@classformatter
 class RunExperimentResults(AttributeWrapper):
     """The results of running an experiment.
 
@@ -180,34 +332,45 @@ class RunExperimentResults(AttributeWrapper):
         self,
         data: dict[str, AcquiredResult],
         neartime_callbacks: dict[str, list[Any]] | None = None,
-        errors: list[tuple[list[int], str, str]] | None = None,
+        errors: ErrorList | None = None,
     ):
         super().__init__(data)
         self._neartime_callbacks = neartime_callbacks or {}
-        self.errors = errors
+        self._errors = errors or []
         self._key_cache.update(["neartime_callbacks", "errors"])
 
     @property
-    def neartime_callbacks(self) -> dict[str, list[Any]]:
+    def neartime_callbacks(self) -> AttributeWrapper:
         """The results of the near-time user callbacks."""
         return AttributeWrapper(self._neartime_callbacks)
 
-    def __getitem__(self, key: str) -> AttributeWrapper:
+    @property
+    def errors(self) -> ErrorList:
+        """The errors that occured during running the experiment."""
+        return self._errors
+
+    def __getitem__(self, key: object) -> AttributeWrapper | ErrorList | object:
         if key == "neartime_callbacks":
             return AttributeWrapper(self._neartime_callbacks)
         if key == "errors":
-            return AttributeWrapper(self._errors)
+            return self.errors or []
         return super().__getitem__(key)
 
-    def __str__(self) -> str:
-        return str(
-            {
-                self._get_subkeys(k): v
-                for k, v in self._data.items()
-                if k.startswith(self._path)
-            }
-            | {
-                "neartime_callbacks": self._neartime_callbacks,
-                "errors": self.errors,
-            },
+    def __dir__(self):
+        return super().__dir__() + list(self._key_cache)
+
+    def _as_str_dict(self) -> dict[str, Any]:
+        return {
+            key: (attr._as_str_dict() if isinstance(attr, AttributeWrapper) else attr)
+            for key, attr in ((key, getattr(self, key)) for key in self._key_cache)
+        } | {
+            "neartime_callbacks": self._neartime_callbacks,
+            "errors": self.errors,
+        }
+
+    def __repr__(self) -> str:
+        return (
+            f"RunExperimentResults(data={self._data!r}, "
+            f"near_time_callbacks={self._neartime_callbacks!r}, errors={self.errors!r},"
+            f" path = {self._path!r}, separator={self._separator!r})"
         )
--- tests/tasks/test_datatypes.py
+++ tests/tasks/test_datatypes.py
@@ -1,8 +1,24 @@
 """Tests for the datatypes module."""
 
+from __future__ import annotations
+
+from contextlib import nullcontext as does_not_raise
+from textwrap import dedent
+
 import pytest
 
+import laboneq_applications.tasks.datatypes as tasks_datatypes
 from laboneq_applications.tasks.datatypes import AttributeWrapper, RunExperimentResults
+
+
+class PrettyPrinter:
+    """A class to test the pretty printer."""
+
+    def __init__(self) -> None:
+        self.pretty_text: str | None = None
+
+    def text(self, text: str):
+        self.pretty_text = text
 
 
 def test_attribute_wrapper():
@@ -13,42 +29,89 @@ def test_attribute_wrapper():
         "result/q0": 345,
     }
     wrapper = AttributeWrapper(data)
-    assert (
-        str(wrapper)
-        == "{'cal_trace.q0.g': 12345, 'cal_trace.q1.g': 2345, 'result.q0': 345}"
-        == repr(wrapper)
-    )
-    assert (
-        str(wrapper.cal_trace)
-        == "{'q0.g': 12345, 'q1.g': 2345}"
-        == repr(wrapper.cal_trace)
-    )
+    assert wrapper._as_str_dict() == {
+        "cal_trace": {"q0": {"g": 12345}, "q1": {"g": 2345}},
+        "result": {"q0": 345},
+    }
+
     assert "q0" in dir(wrapper.cal_trace)
+    assert isinstance(wrapper.cal_trace, AttributeWrapper)
+    assert isinstance(wrapper.cal_trace.q0, AttributeWrapper)
     assert wrapper.cal_trace.q0.g == 12345
     subtree = wrapper.cal_trace
+    assert isinstance(subtree, AttributeWrapper)
     assert len(subtree) == 2
-    keys = subtree.keys()
+    keys = subtree._keys()
     assert set(keys) == {"q0", "q1"}
     assert set(iter(subtree)) == {"q0", "q1"}
     assert "q0" in subtree
     assert "q0" in keys
     assert "not_a_key" not in subtree
     assert "not_a_key" not in keys
-    assert {v["g"] for v in subtree.values()} == set(data.values()) - {345}
+    assert {v["g"] for v in subtree._values()} == set(data.values()) - {345}
+    items = subtree._items()
+    assert {k for k, _ in items} == set(keys)
+    assert [v for _, v in items] == list(subtree._values())
     subsubtree = subtree.q0
+    assert isinstance(subsubtree, AttributeWrapper)
     assert subsubtree.g == 12345
-    assert list(subsubtree.values()) == [12345]
-    assert subtree["q1"]["g"] == 2345
-    assert wrapper["cal_trace"].q0["g"] == 12345
+    assert list(subsubtree._values()) == [12345]
+    subtree_q1 = subtree["q1"]
+    wrapper_caltrace = wrapper["cal_trace"]
+    assert isinstance(subtree_q1, AttributeWrapper)
+    assert isinstance(wrapper_caltrace, AttributeWrapper)
+    assert isinstance(wrapper_caltrace.q0, AttributeWrapper)
+    assert subtree_q1["g"] == 2345
+    assert wrapper_caltrace.q0["g"] == 12345
 
+    result = wrapper.result
+    assert isinstance(result, AttributeWrapper)
     with pytest.raises(
         AttributeError,
         match="Key 'result/q1' not found in the data.",
     ):
-        _ = wrapper.result.q1
+        _ = result.q1
 
     with pytest.raises(AttributeError):
         _ = wrapper.results
+    with pytest.raises(TypeError, match=r"Key 1 has to be of type str\."):
+        _ = wrapper[1]
+
+
+def test_attribute_wrapper_formatting():
+    assert tasks_datatypes.use_rich_pprint
+    try:
+        data = {"cal_trace/q1/g": 2345}
+        wrapper = AttributeWrapper(data)
+        assert f"{wrapper}" == "{'cal_trace': {'q1': {'g': 2345}}}"
+        assert f"{wrapper.cal_trace}" == "{'q1': {'g': 2345}}"
+        assert (
+            str(wrapper) == "{\n│   'cal_trace': {\n│   │   "
+            "'q1': {\n│   │   │   'g': 2345\n│   │   }\n│   }\n}\n"
+        )
+        assert str(wrapper.cal_trace) == "{\n│   'q1': {\n│   │   'g': 2345\n│   }\n}\n"
+        assert (
+            repr(wrapper) == "AttributeWrapper(data={'cal_trace/q1/g': 2345}, "
+            "path='', separator='/')"
+        )
+        assert (
+            repr(wrapper.cal_trace)
+            == "AttributeWrapper(data={'cal_trace/q1/g': 2345}, "
+            "path='cal_trace', separator='/')"
+        )
+        p = PrettyPrinter()
+        wrapper._repr_pretty_(p, None)
+        assert str(wrapper) == p.pretty_text
+        assert isinstance(wrapper.cal_trace, AttributeWrapper)
+        wrapper.cal_trace._repr_pretty_(p, None)
+        assert str(wrapper.cal_trace) == p.pretty_text
+        tasks_datatypes.use_rich_pprint = False
+        from pprint import pprint as pprint_pprint
+
+        tasks_datatypes.pprint_pprint = pprint_pprint
+        assert str(wrapper) == "{'cal_trace': {'q1': {'g': 2345}}}\n"
+    finally:
+        tasks_datatypes.use_rich_pprint = True
 
 
 def test_run_experiment_results():
@@ -56,7 +119,7 @@ def test_run_experiment_results():
     neartime_results = {"nt1": 12345, "nt2": {"a": "b", "c": "d"}}
     errors = [(0, "error1", "error1 message"), (1, "error2", "error2 message")]
     results = RunExperimentResults(data, neartime_results, errors)
-    assert set(results.keys()) == {
+    assert set(results._keys()) == {
         "cal_trace",
         "sweep_data",
         "neartime_callbacks",
@@ -68,15 +131,147 @@ def test_run_experiment_results():
         for k in ["cal_trace", "sweep_data", "neartime_callbacks", "errors"]
     )
     assert results.errors is errors
+    assert results["errors"] is errors
+    assert isinstance(results.neartime_callbacks, AttributeWrapper)
+    assert results["neartime_callbacks"] == results.neartime_callbacks
     assert "nt1" in results.neartime_callbacks
-    assert results.neartime_callbacks.nt2["a"] == "b"
+    assert isinstance(results.neartime_callbacks, AttributeWrapper)
+    assert results.neartime_callbacks.nt2 == neartime_results["nt2"]
+    result_str = dedent("""\
+            │   'sweep_data': {
+            │   │   'q0': 345
+            │   }""")
+    q1_str = dedent("""\
+            │   │   'q1': {
+            │   │   │   'g': 2345
+            │   │   }""")
+    q0_str = dedent("""\
+            │   │   'q0': {
+            │   │   │   'g': 12345
+            │   │   }""")
+    error_str = dedent("""\
+            │   'errors': [
+            │   │   (
+            │   │   │   0,
+            │   │   │   'error1',
+            │   │   │   'error1 message'
+            │   │   ),
+            │   │   (
+            │   │   │   1,
+            │   │   │   'error2',
+            │   │   │   'error2 message'
+            │   │   )
+            │   ]""")
+    nt1_str = dedent("""\
+            │   │   'nt1': 12345""")
+    nt2_str = dedent("""\
+            │   │   'nt2': {
+            │   │   │   'a': 'b',
+            │   │   │   'c': 'd'
+            │   │   }""")
+    s = str(results)
+    assert result_str in s
+    assert q1_str in s
+    assert q0_str in s
+    assert error_str in s
+    assert nt1_str in s
+    assert nt2_str in s
+
+
+def test_result_formatting():
+    data = {"cal_trace/q0/g": 12345}
+    neartime_results = {"nt2": {"a": "b", "c": "d"}}
+    errors = [(0, "error1", "error1 message")]
+    results = RunExperimentResults(data, neartime_results, errors)
+
+    assert "'cal_trace': {'q0': {'g': 12345}}" in f"{results}"
+    assert "'errors': [(0, 'error1', 'error1 message')]" in f"{results}"
+    assert "'neartime_callbacks': {'nt2': {'a': 'b', 'c': 'd'}}" in f"{results}"
     assert (
-        str(results)
-        == "{'cal_trace.q0.g': 12345, 'cal_trace.q1.g': 2345, 'sweep_data.q0': 345, "
-        f"'neartime_callbacks': {neartime_results}, 'errors': {errors}}}"
+        "'cal_trace': {\n│   │   'q0': {\n│   │   │   'g': 12345\n│   │   }\n│   }"
+        in str(results)
+    )
+    assert (
+        "'errors': [\n│   │   (\n│   │   │   0,\n│   │   │   'error1',\n│   │   │   "
+        "'error1 message'\n│   │   )\n│   ]" in str(results)
+    )
+    assert (
+        "'neartime_callbacks': {\n│   │   'nt2': {\n│   │   │   'a': 'b',\n│   │   │   "
+        "'c': 'd'\n│   │   }\n│   }" in str(results)
     )
     assert (
         repr(results)
-        == "{'cal_trace.q0.g': 12345, 'cal_trace.q1.g': 2345, 'sweep_data.q0': 345, "
-        f"'neartime_callbacks': {neartime_results!r}, 'errors': {errors}}}"
+        == "RunExperimentResults(data={'cal_trace/q0/g': 12345}, near_time_callbacks="
+        "{'nt2': {'a': 'b', 'c': 'd'}}, errors=[(0, 'error1', 'error1 message')], "
+        "path = '', separator='/')"
     )
+    assert f"{results.cal_trace}" == "{'q0': {'g': 12345}}"
+    assert (
+        repr(results.cal_trace)
+        == "AttributeWrapper(data={'cal_trace/q0/g': 12345}, path='cal_trace', "
+        "separator='/')"
+    )
+
+    p = PrettyPrinter()
+    results._repr_pretty_(p, None)
+    assert str(results) == p.pretty_text
+    results.cal_trace._repr_pretty_(p, None)
+    assert str(results.cal_trace) == p.pretty_text
+
+
+@pytest.mark.parametrize(
+    ("handles", "k1", "k2"),
+    [
+        (["a", "a.", "ab", "aa/a", "aa/b", "aa/b.", "aa/b../a"], None, None),
+        (["a"], None, None),
+        (["a/a"], None, None),
+        ([], None, None),
+        (
+            ["a", "a.", "ab", "aa/a", "aa/b", "aa/b.", "a./b../a"],
+            r"a\.",
+            r"a\./b\.\./a",
+        ),
+        (["a", "a/a"], "a", "a/a"),
+        (["a/a/a", "a/b", "a/a/a/a"], "a/a/a", "a/a/a/a"),
+        (["a/a.", "a/./a", "a/a./a/a"], r"a/a\.", r"a/a\./a/a"),
+        (
+            ["q0/data", "q0/data/active_reset/a", "q0/data/active_reset/b"],
+            "q0/data",
+            "q0/data/active_reset/.",
+        ),
+    ],
+)
+def test_check_unique_key_classes(handles: list[str], k1: str | None, k2: str | None):
+    d = {k: None for k in handles}
+    if k1 is None:
+        with does_not_raise():
+            AttributeWrapper(d)
+    else:
+        with pytest.raises(ValueError, match=f"Key '{k1}' is a prefix of '{k2}'."):
+            AttributeWrapper(d)
+
+
+@pytest.mark.parametrize(
+    ("handles", "keys"),
+    [
+        (["__init___", "__init_/_"], None),
+        (["__init__"], "{'__init__'}"),
+        (["_add_path"], "{'_add_path'}"),
+        (["__len__/a"], "{'__len__'}"),
+        (["__len__/a/b"], "{'__len__'}"),
+        (["a/__len__/b"], "{'__len__'}"),
+        (["a/__len__"], "{'__len__'}"),
+        (
+            ["__getitem__", "_as_str_dict"],
+            "({'__getitem__', '_as_str_dict'})|({'_as_str_dict', '__getitem__'})",
+        ),
+    ],
+)
+def test_key_is_attribute(handles: list[str], keys: str | None):
+    d = {k: None for k in handles}
+    if keys is None:
+        with does_not_raise():
+            AttributeWrapper(d)
+    else:
+        with pytest.raises(ValueError, match=f"Handles {keys} aren't allowed names\\."):
+            AttributeWrapper(d)
