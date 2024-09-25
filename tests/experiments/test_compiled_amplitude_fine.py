88bf6ab8f7a4b3322906ec8e37f2dbdad7636bd9
hsyeo <hsyeo@kriss.re.kr>
2024-09-25 17:18:53 +0200

    max_events=10000, add repetitions for practical case

--- tests/experiments/test_compiled_amplitude_fine.py
+++ tests/experiments/test_compiled_amplitude_fine.py
@@ -1,4 +1,4 @@
-"""Tests for the compiled amplitude_fine experiment 
+"""Tests for the compiled amplitude_fine experiment
 using the testing utilities provided by the LabOne Q Applications Library.
 """
 
@@ -22,18 +22,14 @@ def on_system_grid(time, system_grid=8):
     return round(time_ns * 1e-9, 12)
 
 
-@pytest.mark.parametrize("repetitions", [np.arange(1, 16, 1),np.arange(1, 25, 1),np.arange(1, 50, 1)])
-@pytest.mark.parametrize("num_qubits", [1])
+@pytest.mark.parametrize("repetitions", [np.arange(1, 3, 1), np.arange(1, 52, 25)])
+@pytest.mark.parametrize("num_qubits", [1, 2])
 @pytest.mark.parametrize(
     "transition, cal_states",
-    [("ge", "ge")], #, ("ef", "ef")
+    [("ge", "ge"), ("ef", "ef")],
 )
 class TestAmplitudeFine:
-    """Test for fine-amplitude on a single/two qubit
-
-    `two_tunable_transmon_platform` is a pytest fixture that is automatically
-    imported into the test function.
-    """
+    """Test for fine-amplitude on a single/two qubit"""
 
     @pytest.fixture(autouse=True)
     def _setup(self, two_tunable_transmon_platform):
@@ -44,7 +40,7 @@ class TestAmplitudeFine:
     @pytest.fixture(autouse=True)
     def _set_options(self, transition, cal_states):
         self.options = amplitude_fine.options()
-        self.options.create_experiment.count = 1  # No need to be parameterized here
+        self.options.create_experiment.count = 5  # No need to be parameterized here
         self.options.create_experiment.transition = transition
         self.options.create_experiment.cal_states = cal_states
         self.options.do_analysis = False
@@ -54,36 +50,34 @@ class TestAmplitudeFine:
         self.count = self.options.create_experiment.count
         self.use_cal_traces = self.options.create_experiment.use_cal_traces
 
-
     @pytest.fixture(autouse=True)
     def create_fine_amplitude_verifier(self, repetitions, num_qubits):
-        self.qubits[0].parameters.ge_drive_length = 32e-9
-
         res = amplitude_fine.experiment_workflow(
             session=self.platform.session(do_emulation=True),
             qpu=self.qpu,
             qubits=[self.qubits[i] for i in range(num_qubits)],
-            amplification_qop= "x180",
-            target_angle= 0,
-            phase_offset = 0.0,
+            amplification_qop="x180",
+            target_angle=0,
+            phase_offset=0.0,
             repetitions=[repetitions for i in range(num_qubits)],
             parameter_to_update="x180",
             options=self.options,
         ).run()
         self.num_qubits = num_qubits
         self.verifier = CompiledExperimentVerifier(
-            res.tasks["compile_experiment"].output
+            res.tasks["compile_experiment"].output, max_events=10000
         )
+        return self.verifier
 
     def test_pulse_count_drive(self, repetitions):
         """Test the total number of drive pulses with given repetitions"""
-        
+
         if self.transition == "ge":
-            expected_ge = self.count * (len(repetitions)+ np.sum(repetitions))
+            expected_ge = self.count * (len(repetitions) + np.sum(repetitions))
             expected_ef = 0
         elif self.transition == "ef":
             expected_ge = self.count * len(repetitions)
-            expected_ef = self.count * (len(repetitions)+np.sum(repetitions))
+            expected_ef = self.count * (len(repetitions) + np.sum(repetitions))
 
         if self.cal_states == "ge":
             expected_ge += self.count * int(self.use_cal_traces)
@@ -101,7 +95,6 @@ class TestAmplitudeFine:
                 expected_ef,
             )
 
-    # @pytest.mark.skip("test another module")
     def test_pulse_count_measure_acquire(self, repetitions):
         """Test the total number of meausre and acquire pulses with given repetitions"""
 
