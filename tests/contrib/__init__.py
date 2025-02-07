3e08475d0179f594579bfd2bb18fc6e6dfb64bbf
Chi-Huan Nguyen <chihuan.nguyen@zhinst.com>
2025-02-07 15:09:14 +0000

    Add a CI job for running smoke tests of contrib experiments

---
+++ tests/contrib/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2025 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+"""A collection of smoke tests for contrib experiments.
+
+These tests are meant to ensure that the experiments are correctly defined
+and can be run without critical errors.
+
+However, they do not test the correctness of the results.
+"""
---
+++ tests/contrib/test_time_rabi.py
@@ -0,0 +1,27 @@
+# Copyright 2025 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""Smoke-tests for the time_rabi experiments."""
+
+import numpy as np
+
+from laboneq_applications.contrib.experiments import time_rabi
+
+
+class TestTimeRabi:
+    def test_time_rabi(self, two_tunable_transmon_platform):
+        platform = two_tunable_transmon_platform
+        qpu = platform.qpu
+        qubits = platform.qpu.qubits
+        options = time_rabi.experiment_workflow.options()
+        options.do_analysis(True)
+        session = platform.session(do_emulation=True)
+        wf = time_rabi.experiment_workflow(
+            session,
+            qpu,
+            qubits,
+            [np.arange(0.1e-6, 1.05e-6, 0.1e-6), np.arange(0.1e-6, 1.05e-6, 0.1e-6)],
+            options,
+        )
+        wf.run()
