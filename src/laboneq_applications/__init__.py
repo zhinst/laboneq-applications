56b2a11a6388852ed3f5f461f9ca2c2bf9aacfa4
Ali Foroozandeh <ali.foroozandeh@zhinst.com>
2025-07-30 23:43:11 +0200

    Version 2.11.1dev0

--- src/laboneq_applications/__init__.py
+++ src/laboneq_applications/__init__.py
@@ -3,4 +3,4 @@
 
 """LabOne Q Applications Library."""
 
-__version__ = "2.11.0"
+__version__ = "2.11.1dev0"
--- tests/test_pkg.py
+++ tests/test_pkg.py
@@ -7,4 +7,4 @@ import laboneq_applications
 
 
 def test_version():
-    assert laboneq_applications.__version__ == "2.11.0"
+    assert laboneq_applications.__version__ == "2.11.1dev0"
