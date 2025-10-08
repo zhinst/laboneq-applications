fa20ecb5d19ded457cc09240ad16e158e12f976d
Bart Andrews <bart.andrews@zhinst.com>
2025-10-08 13:03:52 +0000

    Add tunable coupler class (QRL-493)

---
+++ src/laboneq_applications/qpu_types/tunable_coupler/__init__.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Zurich Instruments AG
+# SPDX-License-Identifier: Apache-2.0
+
+"""Tunable coupler, parameters and operations."""
+
+__all__ = [
+    "TunableCoupler",
+    "TunableCouplerParameters",
+]
+
+from .coupler_types import TunableCoupler, TunableCouplerParameters
